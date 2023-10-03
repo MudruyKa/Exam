589433614e1efe2163177ef54a138763689c269c
MudruyKa <140792836+MudruyKa@users.noreply.github.com>
2023-10-03 10:15:23 +0000

    Написал код

--- examTask/Program.cs
+++ examTask/Program.cs
@@ -7,3 +7,33 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
+
+Console.Clear();
+
+{
+    // Вводим исходный массив строк с клавиатуры
+    Console.WriteLine("Введите строки, разделенные пробелами:");
+    string input = Console.ReadLine();
+    string[] inputStrings = input.Split();
+
+    // Инициализируем пустой массив для результата, <= 3 символов
+    string[] resultStrings = new string[0];
+
+    // Проходим по каждой строке в исходном массиве
+    foreach (string str in inputStrings)
+    {
+        // Если длина строки меньше или равна 3 символам, добавляем ее в результат
+        if (str.Length <= 3)
+        {
+            Array.Resize(ref resultStrings, resultStrings.Length + 1);
+            resultStrings[resultStrings.Length - 1] = str;
+        }
+    }
+
+    // Выводим результат
+    Console.WriteLine("Результат:");
+    foreach (string str in resultStrings)
+    {
+        Console.WriteLine(str);
+    }
+}
